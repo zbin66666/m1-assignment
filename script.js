1bdbf33f04e8a54e00cd1fbd226eb4317baa8b86
zbin66666 <binz6@uci.edu>
2023-06-15 02:22:18 -0700

    add info box&&move script to external&&set style

---
+++ script.js
@@ -0,0 +1,67 @@
+var photos = [];
+var fileNames = [];
+var imageList = [];
+
+var captionList = [];
+var openCaption = "<figcaption class='caption'>";
+var closeCaption = "</figcaption>";
+var dogNames = ["Tom","Charlie","Daisy","Bella","Buddy","Max","Bailey","Lucy","Cooper","Luna"];
+
+var openDesc = "<p class='description'>";
+var closeDesc = "</p>";
+var petsInfo = [] ;
+
+var dogBreed =["Labrador Retriever","German Shepherd","Golden Retriever","Siberian Husky",
+"American Bulldog","Akita","American Foxhound","Bichon Frise","Boykin Spaniel","Bull Terrier"]
+
+for (var i = 0; i < dogNames.length; i++){
+    fileNames.push("dog" + (i+1));
+    photos.push("<img src='image/" + fileNames[i] + ".jpeg'>");
+    captionList.push(openCaption + dogNames[i] + closeCaption);
+    petsInfo.push(openDesc + dogNames[i] + " is a " + dogBreed[i] + closeDesc);
+}
+
+var petsImage = document.getElementById("petsfigure");
+for (var j = 0; j < photos.length; j++) {
+    var div = document.createElement("div");
+    div.classList.add("petsimage");
+    div.innerHTML = photos[j] + captionList[j] + petsInfo [j]; 
+    petsImage.appendChild(div);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get elements
+    var infoBox = document.getElementById("infoBox");
+    var infoHeading = document.getElementById("infoHeading");
+    var infoText = document.getElementById("infoText");
+    var infoClose = document.getElementById("infoClose");
+
+    // Show info box when description is clicked
+    var descriptions = document.getElementsByClassName("description");
+    for (var i = 0; i < descriptions.length; i++) {
+        descriptions[i].addEventListener("click", function () {
+            // Get caption and description content
+            var caption = this.previousElementSibling.innerHTML;
+            var description = this.innerHTML;
+
+            // Set info box content
+            infoHeading.innerHTML = caption;
+            infoText.innerHTML = description;
+
+            // Show info box
+            infoBox.style.display = "block";
+        });
+    }
+
+    // Hide info box when close link is clicked
+    infoClose.addEventListener("click", function (e) {
+        e.preventDefault();
+        infoBox.style.display = "none";
+    });
+});
+
+
+
+
+
+
